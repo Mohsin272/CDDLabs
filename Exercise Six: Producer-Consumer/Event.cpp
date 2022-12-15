10e84d56a2e76261e3efdf9600cea4d2f253a0b1
Mohsin272 <mohsintahir999@gmail.com>
2022-12-15 23:37:07 +0000

    added doxygen

--- Exercise Six: Producer-Consumer/Event.cpp
+++ Exercise Six: Producer-Consumer/Event.cpp
@@ -15,27 +15,13 @@ Event::Event (){
     std::cout <<"Produced "<<randomChar<<std::endl;
 }
 /**
- * @brief Fuction converts latter to uppercase and prints to screen when it has been consumed
+ * @brief Fuction converts letter to uppercase and prints to screen when it has been consumed.
  * 
  */
 void Event::consume()
 {
     char randomCharUpper = toupper(randomChar);    
-    // if (randomCharUpper=='X'){
-    //     // std::cout << "Number of chracters consumed until X found = "<<count<<std::endl;
-    //     // count =0;
-    //     std::cout <<"*********Found X in Buffer********"<<std::endl;
-    //     std::cout <<"CharBuffer size is "<<Charbuffer.size()<<std::endl;
-    //     std::map<char, int> count;
-    //     for(int i=0; i<Charbuffer.size(); i++) {
-    //         count[Charbuffer[i]]++;
-    //     }
-    //     for(const auto kvp : count) {
-    //         std::cout << kvp.first << " occurs " << kvp.second << " times\n";
-    //     }
-    // }
-    // else{
-         std::cout<<"Consumed " << randomCharUpper <<std::endl;
-    // }
+    std::cout<<"Consumed " << randomCharUpper <<std::endl;
+
 }
 /* Event.h ends here */
